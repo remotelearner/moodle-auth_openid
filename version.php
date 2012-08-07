44a45d2cb90170fefd2269b4344bbb59070fc8c8
Marko Vidberg <marko.vidberg@remote-learner.net>
2012-08-07 11:49:19 -0400

    PDM-249 updated release number

--- version.php
+++ version.php
@@ -13,4 +13,4 @@
 
 $plugin->version  = 2011012000;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2010112200;  // Requires this Moodle 2.0 version
-$plugin->release = '2.2.3';      // ELIS Component Version
+$plugin->release = '2.3.1';      // ELIS Component Version
