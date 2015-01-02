8ec458ef1ecef331521693ace4a58887913553d9
Tyler Bannister <tyler.bannister@remote-learner.net>
2015-01-02 17:23:29 -0500

    HOSSUP-5495 - Updated release to 2.6.1.1

--- version.php
+++ version.php
@@ -10,6 +10,6 @@
  * @package openid
  */
 
-$plugin->version  = 2014010800;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2014010801;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2013111800;  // Requires this Moodle version
-$plugin->release = '2.6.1.0';    // ELIS Component Version
+$plugin->release = '2.6.1.1';    // ELIS Component Version
