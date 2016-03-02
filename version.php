e4c622e0037b5e8eec781682dc5e383c5b5cad4c
Brent Boghosian <brent.boghosian@remote-learner.net>
2016-03-02 15:52:06 -0500

    HOSSUP-6710: Update version release & copyright.

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
  * OpenID module version information
  *
  * @author Brent Boghosian <brent.boghosian@remote-learner.net>
- * @copyright Copyright (c) 2011 Remote-Learner
+ * @copyright Copyright (c) 2011 Onwards Remote-Learner.net Inc. (http://www.remote-learner.net)
  * @author Stuart Metcalfe <info@pdl.uk.com>
  * @copyright Copyright (c) 2007 Canonical
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
@@ -12,5 +12,5 @@
 
 $plugin->version  = 2015102500;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2015101600;  // Requires this Moodle version
-$plugin->release = '3.0.0.0';    // ELIS Component Version
+$plugin->release = '3.0.0.1';    // ELIS Component Version
 $plugin->component = 'auth_openid';
